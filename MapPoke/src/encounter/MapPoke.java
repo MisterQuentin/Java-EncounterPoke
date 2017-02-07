78ae5b0045e97fe5b6c7c132a31e7dbbe911ac60
MisterQuentin <gosselinquentin@orange.fr>
2017-02-07 13:05:54 +0100

    Finissions des requetes, affichage enlever, tous marche en mode console

--- MapPoke/src/encounter/MapPoke.java
+++ MapPoke/src/encounter/MapPoke.java
@@ -5,6 +5,8 @@
  */
 package encounter;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Quentin
@@ -14,9 +16,46 @@ public class MapPoke {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        new Windows();
+       // new Windows();
+       int NumJeux;
+       int NumZone;
+        RequeteSQL affichage = new RequeteSQL();
+        
+        
+        
+        do{
+            affichage.jeux();
+            System.out.println("Selectionner votre jeux");
+            System.out.println(affichage);
+            NumJeux = new Scanner(System.in).nextInt();
+             
+             switch(NumJeux){
+                 case 1:
+                     affichage.zone(NumJeux);
+                     System.out.println("Selectionner votre zone");
+                     System.out.println(affichage);
+                     NumZone = new Scanner(System.in).nextInt();
+                     
+                     switch(NumZone){
+                         case 1:
+                               affichage.pokemon(NumZone);
+                               System.out.println("Les Pokemon présent dans la zone");
+                               System.out.println(affichage);
+                               break;
+                         case 2 :
+                             affichage.pokemon(NumZone);
+                               System.out.println("Les Pokemon présent dans la zone");
+                               System.out.println(affichage);
+                               break;
+                             
+                     }
+                     break;
+                
+                     
+             } 
+        }while(NumJeux != 0);
+        affichage.fermerConnexion();
     }
-    
 }
